d573f19fc710056ab3b1d1d25ab047753944dfad
bkalincz <bkalincz@CNU3079LK9>
2016-09-03 15:12:21 +0200

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.openStackMenuItem = new System.Windows.Forms.MenuItem();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1});
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Index = 0;
+            this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.openStackMenuItem});
+            this.menuItem1.Text = "File";
+            // 
+            // openStackMenuItem
+            // 
+            this.openStackMenuItem.Index = 0;
+            this.openStackMenuItem.Text = "Choose stack files";
+            this.openStackMenuItem.Click += new System.EventHandler(this.clickedChooseStackFiles);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 389);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MainMenu mainMenu1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem openStackMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        FlowLayoutPanel bottomPanel = new FlowLayoutPanel();
+        Panel autoscrollPanel = new Panel();
+        public Form1()
+        {
+            FlowLayoutPanel mainPanel = new FlowLayoutPanel();
+
+            mainPanel.Dock = DockStyle.Fill;
+            mainPanel.AutoSize = true;
+            mainPanel.AutoScroll = true;
+            mainPanel.FlowDirection = FlowDirection.LeftToRight;
+
+            bottomPanel.AutoSize = true;
+            bottomPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            bottomPanel.Dock = DockStyle.Right;
+
+            autoscrollPanel.AutoScroll = true;
+            autoscrollPanel.Dock = DockStyle.Left;
+
+            mainPanel.Controls.Add(autoscrollPanel);
+            mainPanel.Controls.Add(bottomPanel);
+            this.Controls.Add(mainPanel);
+
+            InitializeComponent();
+        }
+
+        private FlowLayoutPanel recreateFlowPanel()
+        {
+            autoscrollPanel.Controls.Clear();
+            var flowPanel = new FlowLayoutPanel();
+            autoscrollPanel.Controls.Add(flowPanel);
+            flowPanel.AutoSize = true;
+            flowPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            flowPanel.FlowDirection = FlowDirection.TopDown;
+            return flowPanel;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void clickedChooseStackFiles(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            DialogResult result = fbd.ShowDialog();
+
+            var imagePanel = recreateFlowPanel();
+            if (string.IsNullOrWhiteSpace(fbd.SelectedPath)) return;
+            string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+            var images = new List<Image>();
+            Func<PictureBox, PictureBox> setSmallPicBox = pb => {
+                    pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Size = new Size(100, 100);
+                return pb; };
+            foreach (var f in files)
+            {
+                try
+                {
+                    var image = Image.FromFile(f);
+                    images.Add(image);
+                    addPictureBox(image, imagePanel, setSmallPicBox);
+
+                } catch
+                {
+                    continue;
+                }
+            }
+
+            bottomPanel.Controls.Clear();
+            Func<PictureBox, PictureBox> left = pb => {
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Size = new Size(500, 500);
+                pb.Anchor = AnchorStyles.Right;
+                return pb; };
+            Func<PictureBox, PictureBox> right = pb => {
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Size = new Size(500, 500);
+                pb.Anchor = AnchorStyles.Left;
+                return pb;
+            };
+            addPictureBox(createCombinedImage(images), bottomPanel, left);
+            addPictureBox(createDepthMap(images), bottomPanel, right);
+        }
+
+        private Image createDepthMap(List<Image> images)
+        {
+            return images.First();
+        }
+
+        private Image createCombinedImage(List<Image> images)
+        {
+            return images.First();
+        }
+
+        private void addPictureBox(Image img, FlowLayoutPanel flowPanel, Func<PictureBox, PictureBox> setter)
+        {
+            var pb = new PictureBox();
+            pb.Image = img;
+            pb = setter(pb);
+            flowPanel.Controls.Add(pb);
+        }
+    }
+}
